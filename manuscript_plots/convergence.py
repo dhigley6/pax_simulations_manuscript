2162f4efed07c3d6651c4f930e61f4e0745233fd
Daniel Higley <dhigley6@gmail.com>
2020-06-11 16:47:57 -0700

    updated convergence plot

--- manuscript_plots/convergence.py
+++ manuscript_plots/convergence.py
@@ -3,12 +3,43 @@
 
 import numpy as np 
 import matplotlib.pyplot as plt 
+import pandas as pd
+import traceback
+from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
+import pickle
 
 from manuscript_plots import set_plot_params
 set_plot_params.init_paper_small()
+from pax_deconvolve.pax_simulations import simulate_pax
+from pax_deconvolve.deconvolution import assess_convergence
 
 FIGURES_DIR = 'figures'
-REGULARIZATION_PARAMETERS = [0.0028, 0.0046, 0.0077, 0.013] # regularization parameters to plot
+REGULARIZATION_PARAMETERS = [0.0028, 0.0129, 0.1] # regularization parameters to plot
+
+def run_ana():
+    """Run analysis to create data used in this plot
+    """
+    log10_num_electrons = 4.0
+    rixs_model = 'schlappa'
+    photoemission_model = 'ag'
+    num_simulations = 100
+    energy_loss = np.arange(-8, 10, 0.01)
+    regularization_strengths = np.logspace(-3, -1, 10)
+    iterations = 1000
+    impulse_response, pax_spectra, xray_xy = simulate_pax.simulate_from_presets(
+        log10_num_electrons,
+        rixs_model,
+        photoemission_model,
+        num_simulations,
+        energy_loss
+    )
+    assess_convergence.run(
+        impulse_response,
+        pax_spectra,
+        xray_xy,
+        regularization_strengths,
+        iterations
+    )
 
 def make_figure():
     _, axs = plt.subplots(2, 1, sharex=True, figsize=(3.37, 3.5))
@@ -38,7 +69,7 @@ def _load_val_mse():
 
 def _make_deconvolved_mse_plot(ax, data_list):
     for ind, data in enumerate(data_list):
-        ax.loglog(data[:, 1], data[:, 2], label=str(REGULARIZATION_PARAMETERS[ind]))
+        ax.loglog(data[:, 1], data[:, 2], label=str(1000*REGULARIZATION_PARAMETERS[ind]))
 
 def _make_val_mse_plot(ax, data_list):
     min_list = []
@@ -47,16 +78,16 @@ def _make_val_mse_plot(ax, data_list):
         min_list.append(minimum)
     minimum = np.amin(min_list)
     for ind, data in enumerate(data_list):
-        ax.loglog(data[:, 1], data[:, 2]-minimum+10**(-3.5), label=str(REGULARIZATION_PARAMETERS[ind]))
-    ax.axvline(77*4, color='k', linestyle='--')
+        ax.loglog(data[:, 1], data[:, 2]-minimum+10**(-2), label=str(REGULARIZATION_PARAMETERS[ind]))
+    ax.axvline(18*4, color='k', linestyle='--')
 
 def _format_figure(axs):
-    axs[1].annotate('stopping\ncriterion met', (308, 0.106), xytext=(20, 0.106), arrowprops=dict(arrowstyle="->"), verticalalignment='center')
-    axs[0].set_ylim((0.07, 20))
+    axs[1].annotate('stopping\ncriterion met', (18*4, 0.5), xytext=(5, 0.5), arrowprops=dict(arrowstyle="->"), verticalalignment='center')
+    axs[0].set_ylim((0.35, 25))
     axs[1].set_xlim((1, 1E3))
-    axs[0].legend(loc='upper right', ncol=2, title='Regularization\nStrength (eV)')
+    axs[0].legend(loc='upper center', ncol=2, title='Regularization\nStrength (meV)')
     axs[0].set_ylabel('Deconvolved\nMSE (a.u.)')
-    axs[1].set_ylabel('Val. Reconstruction\nMSE-minimum+10$^{-3.5}$ (a.u.)')
+    axs[1].set_ylabel('Val. Reconstruction\nMSE-minimum+10$^{-2}$ (a.u.)')
     axs[1].set_xlabel('Iterations')
     axs[0].text(0.1, 0.05, 'A', fontsize=10, weight='bold', horizontalalignment='center',
                    transform=axs[0].transAxes)
